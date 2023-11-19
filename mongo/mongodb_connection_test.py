6a2806876f5d1dafae1f0ddd7aff5dd8ae44b88f
ejeong0303 <ejeong0303@gmail.com>
2023-11-19 10:55:00 +0900

    Initial commit

---
+++ mongo/mongodb_connection_test.py
@@ -0,0 +1,53 @@
+from pymongo import MongoClient # pymongo 임포트
+client = MongoClient('localhost', 27017) # mongoDB라는 27017 포트로 돌아갑니다.
+db = client.textcrafters.keywords
+
+# insert 배우기
+doc = {'restaurant':'아건','category':'price','keyword':'가성비가 좋아요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'price','keyword':'가성비가 있어요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'price','keyword':'너무 가성비 뿜뿜해요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'price','keyword':'가성비가 좋은 편이예요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'price','keyword':'가성비가 좋은 것 같아요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'taste','keyword':'너무 맛있어요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'taste','keyword':'정말 맛있어요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'taste','keyword':'맛있게 먹었어요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'taste','keyword':'너무 맛있었어요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'taste','keyword':'짱 맛나요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'아건','category':'taste','keyword':'짱 맛있었어요'}
+db.insert_one(doc)
+
+
+doc = {'restaurant':'조용한 식탁','category':'service','keyword':'사장님이 친절해요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'조용한 식탁','category':'service','keyword':'직원들이 친절해요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'조용한 식탁','category':'service','keyword':'사장님이 너무 친절해요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'조용한 식탁','category':'service','keyword':'전반적으로 친절해요'}
+db.insert_one(doc)
+
+doc = {'restaurant':'조용한 식탁','category':'service','keyword':'외국인 직원이 친절해요'}
+db.insert_one(doc)
---
+++ mongo/similarity_check.py
@@ -0,0 +1,50 @@
+import difflib
+from pymongo import MongoClient
+
+def calculate_similarity(keyword1, keyword2):
+    answer_bytes = bytes(keyword1, 'utf-8')
+    input_bytes = bytes(keyword2, 'utf-8')
+    sm = difflib.SequenceMatcher(None, list(answer_bytes), list(input_bytes))
+    return sm.ratio()
+
+def integrate_keywords(collection, restaurant, category, keyword1, keyword2):
+    # Keep keyword1 and delete keyword2
+    result = collection.delete_one({'restaurant': restaurant, 'category': category, 'keyword': keyword2})
+    print(f"Deleted {keyword2}, result: {result.deleted_count}")
+
+# Connect to MongoDB
+client = MongoClient('localhost', 27017)
+db = client.textcrafters
+collection = db.keywords
+
+# Fetch unique restaurants
+restaurants = collection.distinct('restaurant')
+print(f"Restaurants: {restaurants}")
+
+# Categories to process
+categories = ['price', 'service', 'taste', 'atmosphere']
+similarity_threshold = 0.7
+
+for restaurant in restaurants:
+    for category in categories:
+        print(f"Processing restaurant: {restaurant}, category: {category}")
+        while True:
+            keywords = collection.find({'restaurant': restaurant, 'category': category})
+            keyword_list = [doc['keyword'] for doc in keywords]
+            print(f"Keywords: {keyword_list}")
+
+            integrated = False
+            for i, keyword1 in enumerate(keyword_list):
+                for keyword2 in keyword_list[i+1:]:
+                    similarity = calculate_similarity(keyword1, keyword2)
+                    print(f"Similarity between '{keyword1}' and '{keyword2}': {similarity}")
+                    if similarity >= similarity_threshold:
+                        integrate_keywords(collection, restaurant, category, keyword1, keyword2)
+                        integrated = True
+                        break
+
+                if integrated:
+                    break
+
+            if not integrated:
+                break
